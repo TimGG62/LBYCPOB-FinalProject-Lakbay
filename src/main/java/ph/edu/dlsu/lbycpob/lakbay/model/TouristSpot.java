bea73810da7c75f91b426015939cdd5b59390ec0
ken-xv <ken_rasing@dlsu.edu.ph>
2026-08-18 23:17:45 +0800

    Added constructors and getters to both set up destination details and retrieve them

--- src/main/java/ph/edu/dlsu/lbycpob/lakbay/model/TouristSpot.java
+++ src/main/java/ph/edu/dlsu/lbycpob/lakbay/model/TouristSpot.java
@@ -14,4 +14,33 @@ public class TouristSpot {
     private double rating;
     private String imageFileName;
 
+    // UNDERSTAND: Initializes all the specific details of a tourist destination.
+    public TouristSpot(String id, String name, String countryOrLocation, String scope,
+                       String airportCode, String description, String distance,
+                       String flightDuration, double estimatedPrice, double rating, String imageFileName) {
+        this.id = id;
+        this.name = name;
+        this.countryOrLocation = countryOrLocation;
+        this.scope = scope;
+        this.airportCode = airportCode;
+        this.description = description;
+        this.distance = distance;
+        this.flightDuration = flightDuration;
+        this.estimatedPrice = estimatedPrice;
+        this.rating = rating;
+        this.imageFileName = imageFileName;
+    }
+
+    // UNDERSTAND: Getters.
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getCountryOrLocation() { return countryOrLocation; }
+    public String getScope() { return scope; }
+    public String getAirportCode() { return airportCode; }
+    public String getDescription() { return description; }
+    public String getDistance() { return distance; }
+    public String getFlightDuration() { return flightDuration; }
+    public double getEstimatedPrice() { return estimatedPrice; }
+    public double getRating() { return rating; }
+    public String getImageFileName() { return imageFileName; }
 }
