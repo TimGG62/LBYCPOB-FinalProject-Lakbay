c717d02df8f5334be9ccfa379696ffdfd1c337f5
ken-xv <ken_rasing@dlsu.edu.ph>
2026-08-18 23:24:26 +0800

    Added FilterService class to filter travel locations according to user budget and preferences.

---
+++ src/main/java/ph/edu/dlsu/lbycpob/lakbay/service/FilterService.java
@@ -0,0 +1,9 @@
+package ph.edu.dlsu.lbycpob.lakbay.service;
+
+import org.springframework.stereotype.Service;
+
+// UNDERSTAND: Filters tourist spots to recommend the top destinations based on user preferences.
+@Service
+public class FilterService {
+
+}
