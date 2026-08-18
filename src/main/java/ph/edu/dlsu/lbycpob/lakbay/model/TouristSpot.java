25bbde650fbc4742414b6abadca5920feb68d506
ken-xv <ken_rasing@dlsu.edu.ph>
2026-08-18 23:16:25 +0800

    Added TouristSpot class to store travel destination info, including ratings, location, and pricing.

---
+++ src/main/java/ph/edu/dlsu/lbycpob/lakbay/model/TouristSpot.java
@@ -0,0 +1,17 @@
+package ph.edu.dlsu.lbycpob.lakbay.model;
+
+// UNDERSTAND: Holds details about a travel destination, including location, pricing, and ratings.
+public class TouristSpot {
+    private String id;
+    private String name;
+    private String countryOrLocation;
+    private String scope;
+    private String airportCode;
+    private String description;
+    private String distance;
+    private String flightDuration;
+    private double estimatedPrice;
+    private double rating;
+    private String imageFileName;
+
+}
