529bd97d95db521931528002b7a29f7f477d9328
TimGG62 <kyle_timothy_cruz@dlsu.edu.ph>
2026-08-19 21:48:38 +0800

    Modified TouristSpotService to include all 10 tourist spots of each place featured

--- src/main/java/ph/edu/dlsu/lbycpob/lakbay/service/TouristSpotService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/lakbay/service/TouristSpotService.java
@@ -12,25 +12,49 @@ public class TouristSpotService {
     private final List<TouristSpot> spots = new ArrayList<>();
 
     public TouristSpotService() {
-        // 8 Domestic Spots
-        spots.add(new TouristSpot("1", "El Nido & Coron Lagoons", "Palawan", "Domestic", "PPS", "Big Lagoon, Kayangan Lake, and Shipwreck Diving", "~580 km", "1.0 - 1.5 hrs", 6500, 4.9, "palawan.jpg"));
-        spots.add(new TouristSpot("2", "Kawasan Falls Canyoneering", "Cebu", "Domestic", "CEB", "Turquoise springs and adventure canyoneering", "~570 km", "1.25 - 1.5 hrs", 5000, 4.7, "kawasan.jpg"));
-        spots.add(new TouristSpot("3", "Panglao Island & Sandbar", "Bohol", "Domestic", "TAG", "Alona Beach, Hinagdanan Cave, and Virgin Island", "~630 km", "1.25 - 1.5 hrs", 5500, 4.6, "panglao.jpg"));
-        spots.add(new TouristSpot("4", "Boracay White Beach", "Boracay", "Domestic", "MPH", "Puka Shell Beach, D'Mall, and crystal clear waters", "~310 km", "1.0 - 1.25 hrs", 5200, 4.9, "boracay.jpg"));
-        spots.add(new TouristSpot("5", "General Luna Cloud 9", "Siargao", "Domestic", "IAO", "World-class surfing boardwalk and tidal rock pools", "~750 km", "2.0 hrs", 8000, 4.8, "siargao.jpg"));
-        spots.add(new TouristSpot("6", "Samal Island Beach Resorts", "Davao", "Domestic", "DVO", "Pearl Farm Beach Resort and Hagimit Falls", "~960 km", "1.75 - 2.0 hrs", 6000, 4.5, "samal.jpg"));
-        spots.add(new TouristSpot("7", "Gigantes Saltwater Lagoon", "Iloilo", "Domestic", "ILO", "Tangke Saltwater Lagoon and Cabugao Gamay Island", "~460 km", "1.0 - 1.25 hrs", 4500, 4.7, "gigantes.jpg"));
-        spots.add(new TouristSpot("8", "Cagsawa Ruins & Mayon", "Bicol", "Domestic", "DRP", "Iconic Mayon Volcano backdrop and ATV tours", "~330 km", "1.0 hr", 4000, 4.6, "mayon.jpg"));
+        addSpot("pal-1", "Puerto Princesa Subterranean River & Honda Bay", "Palawan", "/images/pal-1.jpg", "Domestic");
+        addSpot("pal-2", "Coron Kayangan Lake & Shipwreck Diving", "Palawan", "/images/pal-2.jpg", "Domestic");
+        addSpot("pal-3", "El Nido Big Lagoon & Nacpan Beach", "Palawan", "/images/pal-3.jpg", "Domestic");
+        addSpot("pal-4", "San Vicente Long Beach & Port Barton", "Palawan", "/images/pal-4.jpg", "Domestic");
+        addSpot("pal-5", "Cuyo Fort & Capusan Beach Kitesurfing", "Palawan", "/images/pal-5.jpg", "Domestic");
+        addSpot("pal-6", "Taytay Fuerza de Santa Isabel", "Palawan", "/images/pal-6.jpg", "Domestic");
+        addSpot("pal-7", "Roxas Modessa Island & Umalacan Falls", "Palawan", "/images/pal-7.jpg", "Domestic");
+        addSpot("pal-8", "Quezon Tabon Caves Archeological Site", "Palawan", "/images/pal-8.jpg", "Domestic");
+        addSpot("pal-9", "Balabac Islands Onuk Island & Sandbanks", "Palawan", "/images/pal-9.jpg", "Domestic");
+        addSpot("pal-10", "Linapacan Eli Rock & Crystal Diving Waters", "Palawan", "/images/pal-10.jpg", "Domestic");
 
-        // 8 Regional International Spots
-        spots.add(new TouristSpot("9", "Taipei 101 & Shilin Market", "Taiwan", "International", "TPE", "Chiang Kai-shek Memorial and Shilin Night Market", "~1,200 km", "2.0 - 2.5 hrs", 10500, 4.7, "taipei.jpg"));
-        spots.add(new TouristSpot("10", "Ha Long Bay & Hanoi Old Quarter", "Vietnam", "International", "HAN", "Hoan Kiem Lake, Old Quarter, and limestone cruises", "~1,600 km", "3.0 - 3.5 hrs", 12000, 4.8, "hanoi.jpg"));
-        spots.add(new TouristSpot("11", "Petronas Towers & Batu Caves", "Malaysia", "International", "KUL", "Kuala Lumpur skyline, Merdeka Square, and culture", "~2,400 km", "3.5 - 4.0 hrs", 12500, 4.6, "kualalumpur.jpg"));
-        spots.add(new TouristSpot("12", "Ubud Monkey Forest & Uluwatu", "Indonesia", "International", "DPS", "Sacred Monkey Forest and Tegallalang Rice Terraces", "~2,800 km", "4.0 - 4.5 hrs", 15000, 4.9, "bali.jpg"));
-        spots.add(new TouristSpot("13", "Grand Palace & Phi Phi Islands", "Thailand", "International", "BKK", "Wat Arun, Chatuchak Market, and Railay Beach", "~2,300 km", "3.5 - 4.0 hrs", 13000, 4.8, "bangkok.jpg"));
-        spots.add(new TouristSpot("14", "Shibuya Crossing & Senso-ji", "Japan", "International", "NRT", "Tokyo Skytree, Shinjuku nightlife, and temples", "~3,000 km", "4.0 - 4.5 hrs", 18500, 4.9, "tokyo.jpg"));
-        spots.add(new TouristSpot("15", "Gyeongbokgung & Myeongdong", "South Korea", "International", "ICN", "N Seoul Tower, Bukchon Village, and street food", "~2,600 km", "3.5 - 4.0 hrs", 16000, 4.8, "seoul.jpg"));
-        spots.add(new TouristSpot("16", "The Bund & Shanghai Disneyland", "China", "International", "PVG", "Yu Garden, modern skyline, and historic waterfronts", "~1,800 km", "2.5 - 4.5 hrs", 14500, 4.6, "shanghai.jpg"));
+        addSpot("ceb-1", "Mactan Shrine & Luxury Beach Resorts", "Cebu", "/images/ceb-1.jpg", "Domestic");
+        addSpot("ceb-2", "Cebu City Basilica & Temple of Leah", "Cebu", "/images/ceb-2.jpg", "Domestic");
+        addSpot("ceb-3", "Oslob Whale Shark Sanctuary & Sumilon Island", "Cebu", "/images/ceb-3.jpg", "Domestic");
+        addSpot("ceb-4", "Badian Kawasan Falls Canyoneering", "Cebu", "/images/ceb-4.jpg", "Domestic");
+        addSpot("ceb-5", "Moalboal Sardine Run Reef & Panagsama Beach", "Cebu", "/images/ceb-5.jpg", "Domestic");
+        addSpot("ceb-6", "Bantayan Island Kota Beach & Virgin Island", "Cebu", "/images/ceb-6.jpg", "Domestic");
+        addSpot("ceb-7", "Camotes Islands Santiago Bay & Bukilat Cave", "Cebu", "/images/ceb-7.jpg", "Domestic");
+        addSpot("ceb-8", "Malapascua Island Thresher Shark Diving", "Cebu", "/images/ceb-8.jpg", "Domestic");
+        addSpot("ceb-9", "Sibonga Simala Monastery Church", "Cebu", "/images/ceb-9.jpg", "Domestic");
+        addSpot("ceb-10", "Aloguinsan Bojo River Eco-Cultural Tour", "Cebu", "/images/ceb-10.jpg", "Domestic");
+
+        addSpot("boh-1", "Panglao Island Alona Beach & Hinagdanan Cave", "Bohol", "/images/boh-1.jpg", "Domestic");
+        addSpot("boh-2", "Carmen Chocolate Hills Viewing Deck", "Bohol", "/images/boh-2.jpg", "Domestic");
+        addSpot("boh-3", "Corella Philippine Tarsier Sanctuary", "Bohol", "/images/boh-3.jpg", "Domestic");
+        addSpot("boh-4", "Loboc River Cruise & Man-Made Forest", "Bohol", "/images/boh-4.jpg", "Domestic");
+        addSpot("boh-5", "Tagbilaran City Sandugo Blood Compact Shrine", "Bohol", "/images/boh-5.jpg", "Domestic");
+        addSpot("boh-6", "Anda Quinale White Beach & Cabagnow Cave", "Bohol", "/images/boh-6.jpg", "Domestic");
+        addSpot("boh-7", "Balicasag Island Marine Sanctuary & Turtles", "Bohol", "/images/boh-7.jpg", "Domestic");
+        addSpot("boh-8", "Dauis Assumption Shrine & Sacred Well", "Bohol", "/images/boh-8.jpg", "Domestic");
+        addSpot("boh-9", "Danao Extreme Adventure Park", "Bohol", "/images/boh-9.jpg", "Domestic");
+        addSpot("boh-10", "Historic Baclayon Church & Museum", "Bohol", "/images/boh-10.jpg", "Domestic");
+
+        addSpot("bor-1", "Boracay White Beach & Willy's Rock", "Boracay", "/images/bor-1.jpg", "Domestic");
+        addSpot("bor-2", "Kalibo Bakhawan Mangrove Eco-Park", "Boracay", "/images/bor-2.jpg", "Domestic");
+        addSpot("bor-3", "Nabas Hurom-Hurom Cold Springs", "Boracay", "/images/bor-3.jpg", "Domestic");
+        addSpot("bor-4", "Buruanga Ariel's Point Cliff Diving", "Boracay", "/images/bor-4.jpg", "Domestic");
+        addSpot("bor-5", "Tangalan Jawili 7-Basin Falls", "Boracay", "/images/bor-5.jpg", "Domestic");
+        addSpot("bor-6", "Tibiao Antique Kawa Hot Bath Experience", "Boracay", "/images/bor-6.jpg", "Domestic");
+        addSpot("bor-7", "Pandan Malumpati Cold Spring & Tubing", "Boracay", "/images/bor-7.jpg", "Domestic");
+        addSpot("bor-8", "Ibajay Century-Old Mangrove Park", "Boracay", "/images/bor-8.jpg", "Domestic");
+        addSpot("bor-9", "Libacao Aklan River Bamboo Rafting", "Boracay", "/images/bor-9.jpg", "Domestic");
+        addSpot("bor-10", "Batan Tinagong Dagat Inlet", "Boracay", "/images/bor-10.jpg", "Domestic");
     }
 
     private void addSpot(String id, String name, String location, String imageUrl, String scope) {
